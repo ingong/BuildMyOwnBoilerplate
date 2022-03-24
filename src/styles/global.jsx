a8efb6db89967860f649cf5ade5f179196f9889a
ingong <insong1216@gmail.com>
2022-03-24 16:46:06 +0900

    style: global style 추가

---
+++ src/styles/global.jsx
@@ -0,0 +1,23 @@
+import { Global, css } from '@emotion/react';
+
+const style = css`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, 'Noto Sans KR', 'Montserrat', BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 62.5%;
+  }
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={style} />;
+};
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export const size = {
+  large: '1.5em',
+  medium: '1.2em',
+  small: '1em',
+};
+
+const theme = {
+  mainColor: '#0000ff',
+  mq: {
+    laptop: `@media only screen and (min-width: ${size.largest})`,
+    tablet: `@media only screen and (min-width: ${size.large})`,
+    mobile: `@media only screen and (min-width: ${size.small})`,
+  },
+};
+
+export default theme;
